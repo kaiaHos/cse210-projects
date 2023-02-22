828673707de5cb87f3d6e5fb071ec94de2595d47
Kaia Hospodarsky <kaia.hospodarsky@gmail.com>
2023-02-22 16:12:00 -0700

    variable name follow Camel formating

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,13 +6,13 @@ class Program
     {
         // This gets the user input for first name
         Console.Write("What is your first name? ");
-        string first_name = Console.ReadLine();
+        string firstName = Console.ReadLine();
 
         // This gets the user input for the last name
         Console.Write("What is your last name? ");
-        string last_name = Console.ReadLine();
+        string lastName = Console.ReadLine();
 
         // THis write out the name with last, first last
-        Console.Write($"YOur name is {last_name}, {first_name} {last_name}");
+        Console.Write($"YOur name is {lastName}, {firstName} {lastName}");
     }
 }
