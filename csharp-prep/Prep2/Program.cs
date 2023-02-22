5473b04d3e04b4292fcf83b7f5888933984c1892
Kaia Hospodarsky <kaia.hospodarsky@gmail.com>
2023-02-22 16:10:52 -0700

    created code to calculate grade off percentage

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,53 @@ using System;
 
 class Program
 {
+    /* A >= 90
+    B >= 80
+    C >= 70
+    D >= 60
+    F < 60*/
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string grade;
+        //Console.WriteLine("Hello Prep2 World!");
+
+        // This gets the unser input for there grade percentage
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int percentage = int.Parse(gradePercentage);
+
+        if (percentage >= 90)
+        {
+            grade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            grade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            grade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+        
+        Console.WriteLine($"Your grade is: {grade}");
+        
+        if (percentage > 70)
+        {
+            Console.WriteLine("Good job passing the class!");
+        }
+        else 
+        {
+            Console.WriteLine("Keeping working on it and next time you'll pass!");
+        }
     }
+
+
 }
