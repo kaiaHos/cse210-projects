3caa07ec32c3f609fe766372e9bfc9d89ad9514f
Kaia Hospodarsky <kaia.hospodarsky@gmail.com>
2023-02-22 15:31:08 -0700

    James Bond introduction

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // This gets the user input for first name
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        // This gets the user input for the last name
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        // THis write out the name with last, first last
+        Console.Write($"YOur name is {last_name}, {first_name} {last_name}");
     }
 }
