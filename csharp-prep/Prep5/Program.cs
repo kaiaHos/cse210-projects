e3a4a8a84cf85a500d9e88c7c019d26ad6a2b9d5
Kaia Hospodarsky <kaia.hospodarsky@gmail.com>
2023-02-22 20:25:51 -0700

    Square number based off user input

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        //Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int squaredNumber = SquareNumber(number);
+        DisplayResult(squaredNumber, name);
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the the squaring program!");
+    }
+    static string PromptUserName()
+    {
+        string userName;
+        Console.Write("Please enter your name: ");
+        userName = Console.ReadLine();
+        return userName;
+    }
+    static int PromptUserNumber()
+    {
+        int userNumber;
+        Console.Write("Please enter your favorite number: ");
+        userNumber = int.Parse(Console.ReadLine());
+        return userNumber;
+
+    }
+    static int SquareNumber(int numberToSquare)
+    {
+        int numberSquared = numberToSquare * numberToSquare;
+        return numberSquared;
+    }
+    static void DisplayResult(int squaredNumber, string name)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
     }
 }
