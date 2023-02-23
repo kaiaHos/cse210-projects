ecdaebbdad9c988a07db8f060d1f47474dfe8d34
Kaia Hospodarsky <kaia.hospodarsky@gmail.com>
2023-02-22 20:06:18 -0700

    append to list, compute average, find maximum

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int sum = 0;
+        int number;
+        int count = 0;
+        int biggest = 0;
+
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number > 0 || number < 0)
+            {
+                sum = sum + number;
+                numbers.Add(number);
+                count = count + 1;
+            }
+
+            if(number > biggest)
+            {
+                biggest = number;
+            }
+            
+        } while (number != 0);
+
+        Console.WriteLine($"The sum is: {sum}");
+        double average = ((double)sum) / (count);
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {biggest}");
+        //Console.WriteLine("Hello Prep4 World!");
     }
 }
