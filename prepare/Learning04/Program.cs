9248c39ae805aac692574851f39e98e697cd9a46
Kaia Hospodarsky <kaia.hospodarsky@gmail.com>
2023-03-14 15:38:30 -0600

    makes assignment summaries, and info

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment fun = new MathAssignment("Tessa Hospodarsky", "fractions", "7.3", "8-19");
+        Console.WriteLine(fun.GetSummary());
+        Console.WriteLine(fun.GetHomeworkList());
+
+        WritingAssignment joy = new WritingAssignment("Tessa Hospodarsky", "Personal Essay", "Listen then Speak");
+        Console.WriteLine(joy.GetSummary());
+        Console.WriteLine(joy.GetWritingInfo());
+        //Console.WriteLine("Hello Learning04 World!");
     }
 }
