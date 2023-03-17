a6358aace7cac8804f01abfa707ab5b68610bbf4
Kaia Hospodarsky <kaia.hospodarsky@gmail.com>
2023-03-17 17:14:38 -0600

    Added the Menu to the program

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        // menu selection
+        Console.WriteLine("1. Breathing"); // 1 New Entry
+        Console.WriteLine("2. Listing"); // 2 Display Journal
+        Console.WriteLine("3. Reflection");// 3 Save Journal
+        Console.WriteLine("4. Quit");// 4 Quit
+
+        switch(int.Parse(Console.ReadLine()))
+        {
+            default: 
+                Console.Write("In switch statement");
+                
+                break;
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+        }
+
+        //Console.WriteLine("Hello Develop04 World!");
     }
 }
