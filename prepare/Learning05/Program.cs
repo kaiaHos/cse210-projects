d4169ae0ab270fc062789af910c383d338acaa4b
Kaia Hospodarsky <kaia.hospodarsky@gmail.com>
2023-03-21 16:53:47 -0600

    Used polymorphism to calculate the area of a shape

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square(7.5, "green");
+        shapes.Add(square);
+        Rectangle rectangle = new Rectangle(3,4,"blue");
+        shapes.Add(rectangle);
+        Circle circle = new Circle(5, "yellow");
+        shapes.Add(circle);
+
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine(shape.GetColor());
+        }
+        
+        //Console.WriteLine("Hello Learning05 World!");
     }
 }
